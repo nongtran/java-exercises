bb0763f9a420f29b4c2f10c4b880b16607012dec
NongTran <thanhnongpy@gmail.com>
2025-05-28 22:04:59 +0700

    order service

---
+++ microservice/order-service/src/main/java/com/nong/microservice/order/service/OrderService.java
@@ -0,0 +1,26 @@
+package com.nong.microservice.order.service;
+
+import com.nong.microservice.order.dto.OrderRequest;
+import com.nong.microservice.order.model.Order;
+import com.nong.microservice.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+
+        orderRepository.save(order);
+    }
+}
